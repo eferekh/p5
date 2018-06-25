1f07a7a1e5e166f497d0fd478fd28940da982879
Elie Ferekh <elieferekh@gmail.com>
2018-06-25 12:22:14 +0300

    Update main.js

--- 3-SteeringBehavior/main.js
+++ 3-SteeringBehavior/main.js
@@ -1,1 +1,17 @@
+let canvas;
+let vehicle;
 
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.addClass("canvas");
+  
+  vehicle = new Vehicle();
+}
+
+function draw() {
+  background(0);
+  
+  vehicle.behavior();
+  vehicle.update();
+  vehicle.show();
+}
