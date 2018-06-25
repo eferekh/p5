1f7762627429e79d161938f83a18adac2ad24586
Elie Ferekh <elieferekh@gmail.com>
2018-06-25 12:44:04 +0300

    Update main.js

--- 4-GravityTowardsCenter-ConstantSpeed/main.js
+++ 4-GravityTowardsCenter-ConstantSpeed/main.js
@@ -1,1 +1,21 @@
+let canvas;
+let vehicles = [];
 
+function setup() {
+  canvas = createCanvas(windowWidth, windowHeight);
+  canvas.addClass("canvas");
+  
+  for (let i = 0; i < 100; i++) {
+    vehicles.push(new Vehicle());
+  }
+}
+
+function draw() {
+  background(0, 30);
+  
+  for (let i = 0; i < vehicles.length; i++) {
+    vehicles[i].behavior();
+    vehicles[i].update();
+    vehicles[i].show();
+  }
+}
