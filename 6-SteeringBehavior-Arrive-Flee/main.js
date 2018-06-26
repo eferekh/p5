23b81029010fe6760ac96277ffdf9696b862c7cb
Elie Ferekh <elieferekh@gmail.com>
2018-06-26 16:22:57 +0300

    Update main.js

--- 6-SteeringBehavior-Arrive-Flee/main.js
+++ 6-SteeringBehavior-Arrive-Flee/main.js
@@ -5,11 +5,13 @@ function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.addClass("canvas");
   
-  
+  vehicle = new Vehicle();
 }
 
 function draw() {
   background(0);
   
-  
+  vehicle.behaviors();
+  vehicle.update();
+  vehicle.show();
 }
