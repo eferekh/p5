1958e924174d1fe0a28fd71f8e600a4be9e7fb29
Elie Ferekh <elieferekh@gmail.com>
2018-11-08 10:11:33 +0200

    Create main.js

---
+++ 8-FireworksToRandomPos-Arrive-Flee/main.js
@@ -0,0 +1,41 @@
+let canvas;
+let gravity;
+let roots = [];
+let fireworks = [];
+
+function setup() {
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.addClass("canvas");
+    gravity = createVector(0, 0.1);
+    roots.push(new Root());
+}
+
+function draw() {
+    background(0);
+
+    for (let i = roots.length - 1; i >= 0; i--) {
+        roots[i].applyForce(gravity);
+        roots[i].update();
+        roots[i].show();
+
+        if (roots[i].reachedTop()) {
+            let newFireworks = roots[i].explode();
+
+            for (let j = 0; j < newFireworks.length; j++) {
+                fireworks.push(newFireworks[j]);
+            }
+
+            roots.splice(i, 1);
+        }
+    }
+
+    for (let i = fireworks.length - 1; i >= 0; i--) {
+        fireworks[i].behaviors();
+        fireworks[i].update();
+        fireworks[i].show();
+
+        if (fireworks[i].isFaded()) {
+            fireworks.splice(i, 1);
+        }
+    }
+}
