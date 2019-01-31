9911f92bb054306f19bdbbf55c96be3b155f4d24
Elie Ferekh <elieferekh@gmail.com>
2019-01-31 18:32:52 +0200

    app.js

--- 12-clickExplode/app.js
+++ 12-clickExplode/app.js
@@ -1,1 +1,29 @@
+let balls = [];
 
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function mousePressed() {
+  for (let i = 0; i < 400; i++) {
+    balls.push(new Ball());
+  }
+}
+
+function draw() {
+  background(0, 40);
+  
+  for (let i = balls.length - 1; i >= 0; i--) {
+    balls[i].behaviors();
+    balls[i].update();
+    balls[i].show();
+    
+    if (balls[i].stoppedMoving()) {
+      balls.splice(i, 1);
+    }
+  }
+  
+  noStroke();
+  fill(0, 0, 0, 255);
+  ellipse(width / 2, height / 2, 5, 5);
+}
